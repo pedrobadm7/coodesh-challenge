3d2d0b92e887b88a96c6364bcef63629448f5a48
Pedro Barros <pedrobars7a@gmail.com>
2023-01-28 13:24:59 -0300

    Setup react-native-vector-icons

--- react-native.config.js
+++ react-native.config.js
@@ -4,5 +4,12 @@ module.exports = {
     ios: {},
     android: {},
   },
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null
+      }
+    }
+  },
   assets: ['./src/assets/fonts/'],
 };
